a85dc393279193ebacf6e2dd1517ffedd6acaee1
lukasz.m.karas@gmail.com <lukasz.m.karas@gmail.com>
2016-03-10 00:03:46 +0100

    First commit - 8 Ball Game

---
+++ magic_8_ball.py
@@ -0,0 +1,25 @@
+import random
+
+def main():
+	print("Magic 8 ball predict the future!")
+
+	# Have the user enter a question
+	question = input("Enter a Yes/No question: ")
+	
+	
+	# Replay whith a random Yes/Maybe/No response
+	
+	rand_value = int(random.random() * 4)
+
+	if rand_value == 0:
+		print("Yes!")
+	elif rand_value == 1:
+		print("Signs point to yes!")
+	elif rand_value == 2:
+		print("Reply hazy. Ask again.")
+	else:
+		print("I don't think so!")
+
+
+if __name__ == "__main__":
+	main()
